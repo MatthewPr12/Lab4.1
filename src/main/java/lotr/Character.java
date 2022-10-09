144afd97cf5d5e8128e1bb27df691e8cfc7d48ac
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-10-09 18:14:16 +0300

    Character done

---
+++ src/main/java/lotr/Character.java
@@ -0,0 +1,41 @@
+package lotr;
+
+import lombok.Getter;
+
+@Getter
+public class Character {
+    private int hp;
+    private int power;
+
+    public Character(int hp, int power){
+        setHp(hp);
+        this.power = power;
+    }
+    public void setHp(int hp){
+        if(hp<0){
+            hp=0;
+        }
+        this.hp = hp;
+    }
+
+    public void setPower(int pw){
+        if(pw<0)pw=0;
+        this.power=pw;
+    }
+    public void kick(Character c){
+        setHp(getHp()-c.getPower());
+    }
+
+    public boolean isAlive(){
+        return getHp()>0;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "hp=" + hp +
+                ", power=" + power +
+                '}';
+    }
+}
